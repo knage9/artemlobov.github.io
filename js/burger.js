9d1b941b0b309c3e04182c8969d33c318c3af539
knage9 <knagh.forall@gmail.com>
2024-03-31 02:17:06 +0300

    add_burger_menu

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-    $(".header_burger").click(function(event){
-        $(".header_burger, nav, .lang, .header_btn").toggleClass('active');
-        $("body").toggleClass("lock")
+// $(document).ready(function(){
+//     $(".header_burger").click(function(event){
+//         $(".header_menu").toggleClass('active');
+//     });
+// });
+const iconmenu = document.querySelector(".header_burger")
+if (iconmenu){
+    const menubody = document.querySelector(".header_menu")
+    iconmenu.addEventListener("click", function(e){
+        document.body.classList.toggle("lock");
+        iconmenu.classList.toggle("active");
+        menubody.classList.toggle("active");
     });
-});
+}
